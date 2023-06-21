fe0e13a90abb063272f2de31d36e5460e9d5820c
Dipali Jagannath Shinde <dipalishinde264@gmail.com>
2023-06-21 15:39:45 +0530

    Add files via upload

---
+++ LambdaFunc/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LambdaFunc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int,int> o1 = (a, b) => a + b;
+            Console.WriteLine(o1(3,4));
+
+            
+        }
+    }
+   
+}
